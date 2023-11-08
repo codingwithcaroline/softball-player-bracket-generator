03edd88bb733917c2f1493acfcee95da9cee13c6
Caroline Kirkconnell <68080286+trumpetck@users.noreply.github.com>
2023-11-08 11:51:45 -0600

    Add files via upload

---
+++ Part3/IfElseExample.java
@@ -0,0 +1,14 @@
+public class IfElseExample
+{
+   public static void main(String[] args)
+   {
+      int num1 = 10, num2 = 10;
+      if (num1 < num2) {
+         System.out.println(num1 + " is < " + num2);
+      }
+      else {
+         System.out.println(num1 + " is >= " + num2);
+      }
+      System.out.println("Done!");
+   }
+}
